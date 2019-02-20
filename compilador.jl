5b843648a6ddbf6817274077f915b123b351483e
neomatrix <josuecevedo@gmail.com>
2019-02-20 14:31:01 -0600

    analizador lexico terminado, segun yo

--- compilador.jl
+++ compilador.jl
@@ -5,27 +5,41 @@ texto = String(take!(f))
 cantidad_caracteres = length(texto)
 indice=1
 
+mutable struct token
+tipo
+lexema
+end
+
+arreglo_tokens=[]
+
 while(indice<= cantidad_caracteres)
 	global indice
+	global arreglo_tokens
 	try
 		if (texto[indice]== ' ' || texto[indice]== '\t' || texto[indice]== '\n')
            indice=indice
 
 		elseif texto[indice]== '?'
-			println("\tsimbolo_de_?\t?")
+			#println("\tsimbolo_de_?\t?")
+			push!(arreglo_tokens,token("simbolo_de_?","?"))
 
 		elseif texto[indice]== '¿'
-			println("\tsimbolo_de_¿\t¿")
+			#println("\tsimbolo_de_¿\t¿")
+			push!(arreglo_tokens,token("simbolo_de_¿","¿"))
+
 
 		elseif texto[indice]== '+'
-			println("\t  operador     +")
+			#println("\t  operador     +")
+			push!(arreglo_tokens,token("operador","+"))
 
 		elseif texto[indice]== '='
 			if(texto[indice+1]== '=')
-				println("\tcomparacion     ==")
+				#println("\tcomparacion     ==")
+			    push!(arreglo_tokens,token("comparacion","=="))
 				indice=indice+1
 			else
-				println("\t asignacion    =")
+				#println("\t asignacion    =")
+				push!(arreglo_tokens,token("asignacion","="))
 			end
 
 		elseif('0'<= texto[indice] <= '9' )
@@ -35,7 +49,23 @@ while(indice<= cantidad_caracteres)
 				indice = indice+1
 			end
 			indice = indice-1
-			println("\tnumero entero   "*buffer)
+			#println("\tnumero entero   "*buffer)
+			push!(arreglo_tokens,token("numero_entero",buffer))
+
+		elseif texto[indice]=='#'
+			while(texto[indice]!='\n')
+				indice = indice+1
+			end
+        elseif texto[indice]=='"'
+        	buffer=""
+        	buffer = buffer*texto[indice]
+			indice = indice+1
+			while(texto[indice]!='"')
+				buffer = buffer*texto[indice]
+				indice = indice+1
+			end
+            #println("\tstring   "*buffer*"\"")
+			push!(arreglo_tokens,token("string",buffer))
 
 		elseif 'a'<= texto[indice]<='z'
 			buffer = ""
@@ -45,23 +75,29 @@ while(indice<= cantidad_caracteres)
 			end
 
 			if buffer== "origen"
-				println("   palabra reservada   "*buffer)
+			    #println("   palabra reservada   "*buffer)
+				push!(arreglo_tokens,token("palabra reservada",buffer))
 		    elseif buffer== "inicio"
-				println("   palabra reservada   "*buffer)
+			    #println("   palabra reservada   "*buffer)
+				push!(arreglo_tokens,token("palabra reservada",buffer))
 			elseif buffer== "fin"
-				println("   palabra reservada   "*buffer)
+			    #println("   palabra reservada   "*buffer)
+				push!(arreglo_tokens,token("palabra reservada",buffer))
 			elseif buffer== "si"
-				println("   palabra reservada   "*buffer)
+			    #println("   palabra reservada   "*buffer)
+				push!(arreglo_tokens,token("palabra reservada",buffer))
 			elseif buffer== "imprimir"
-				println("   palabra reservada   "*buffer)
+			    #println("   palabra reservada   "*buffer)
+				push!(arreglo_tokens,token("palabra reservada",buffer))
 			elseif (match(Regex("[a-z]+[0-9]*[a-z]*"),buffer) != nothing)
-				println("  nombre_de_variable   "*buffer)
+				#println("  identificador   "*buffer)
+				push!(arreglo_tokens,token("identificador",buffer))
 		    else
-		    	error("asco tu programa amiga, PALABRA NO RECONOCIDA "* buffer)
+		    	error("ERROR LEXICO, PALABRA NO RECONOCIDA "* buffer)
 			end
 			indice= indice-1
 	    else
-	       error("asco tu programa amiga, SIMBOLO NO RECONOCIDO "* texto[indice])
+	       error("ERROR LEXICO, SIMBOLO NO RECONOCIDO "* texto[indice])
 		end
 		indice= indice+1
 	catch e
@@ -71,4 +107,9 @@ while(indice<= cantidad_caracteres)
 		indice= indice+1
        end
     end
+end
+
+#impresion de los tokens en pantalla (tipo y lexema)
+for token in arreglo_tokens
+    println(token.tipo *"\t"*token.lexema)
 end
